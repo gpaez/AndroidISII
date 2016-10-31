aefdd6f0eb9ba80bdaf268eb182c483f4fd9cb15
Guille <gpaez.net@gmail.com>
2016-10-31 20:51:40 -0300

    nuevos

---
+++ Gestor_Ventas/src/main/java/com/gestorventas/PedidoCobroFragment.java
@@ -0,0 +1,29 @@
+package com.gestorventas;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+
+/**
+ * Created by Guille on 31/10/16.
+ */
+
+public class PedidoCobroFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pedido_cobro, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        int position = FragmentPagerItem.getPosition(getArguments());
+
+    }
+}
